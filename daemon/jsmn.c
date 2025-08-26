0983ef8783ca470d46d7c39961d4c9b519161528
Nikitai <giperdrakon@gmail.com>
2025-08-26 12:08:38 +0300

    fix: warning jsmn

--- daemon/jsmn.c
+++ daemon/jsmn.c
@@ -1,14 +1,6 @@
 // File: daemon/jsmn.c
 #include "jsmn.h"
 
-static int jsmn_eq(const char *a, size_t a_len, const char *b) {
-  size_t i = 0;
-  for (i = 0; i < a_len && b[i] != '\0'; i++) {
-    if (a[i] != b[i]) return 0;
-  }
-  return (i == a_len) && (b[i] == '\0');
-}
-
 static jsmntok_t *jsmn_alloc_token(jsmn_parser *parser, jsmntok_t *tokens,
                                    size_t num_tokens) {
   if (parser->toknext >= num_tokens) return NULL;
@@ -37,25 +29,27 @@ static int jsmn_parse_primitive(jsmn_parser *parser, const char *js,
       case '\t': case '\r': case '\n': case ' ': case ',': case ']': case '}':
         goto found;
       default:
-        if (c < 32 || c == 127) {
+        if ((unsigned char)c < 32 || (unsigned char)c == 127) {
           parser->pos = (unsigned int)start;
           return JSMN_ERROR_INVAL;
         }
     }
   }
-found:
-  jsmntok_t *token = jsmn_alloc_token(parser, tokens, num_tokens);
-  if (token == NULL) {
-    parser->pos = (unsigned int)start;
-    return JSMN_ERROR_NOMEM;
-  }
-  jsmn_fill_token(token, JSMN_PRIMITIVE, start, (int)parser->pos);
+found: {
+    jsmntok_t *token = jsmn_alloc_token(parser, tokens, num_tokens);
+    if (token == NULL) {
+      parser->pos = (unsigned int)start;
+      return JSMN_ERROR_NOMEM;
+    }
+    jsmn_fill_token(token, JSMN_PRIMITIVE, start, (int)parser->pos);
 #ifdef JSMN_PARENT_LINKS
-  token->parent = parser->toksuper;
+    token->parent = parser->toksuper;
 #endif
-  parser->pos--;
-  return 0;
+    parser->pos--;
+    return 0;
+  }
 }
+
 
 static int jsmn_parse_string(jsmn_parser *parser, const char *js,
                              size_t len, jsmntok_t *tokens, size_t num_tokens) {
